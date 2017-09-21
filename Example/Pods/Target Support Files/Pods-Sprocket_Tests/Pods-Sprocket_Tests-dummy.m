0280896ca74db03188946f15a3e3d5051182bf04
wzxjiang <wzxjiang@foxmail.com>
2017-09-21 18:54:53 +0800

    remove /Pods

--- Example/Pods/Target Support Files/Pods-Sprocket_Tests/Pods-Sprocket_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Sprocket_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Sprocket_Tests
-@end
--- Example/Pods/Target Support Files/Sprocket/Sprocket-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Sprocket : NSObject
-@end
-@implementation PodsDummy_Sprocket
-@end
