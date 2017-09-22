f5c8343533a9d50c3e3cc2e824e737844b0cec0d
wzxjiang <wzxjiang@foxmail.com>
2017-09-22 14:20:55 +0800

    add single state monitoring

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -8,6 +8,17 @@ class Tests: XCTestCase {
         case getUp
         case eat
         case sleep
+        
+        var string: String {
+            switch self {
+            case .getUp:
+                return "getUp"
+            case .eat:
+                return "eat"
+            case .sleep:
+                return "sleep"
+            }
+        }
     }
     
     let sprocket = Sprocket<State>(idle: .getUp)
@@ -20,12 +31,6 @@ class Tests: XCTestCase {
             [.getUp] >>> .eat,
             [.eat]   >>> .sleep,
         ]
-
-        sprocket.before { (current, from) in }
-
-        sprocket.on { (current, from) in }
-
-        sprocket.after { (current, from) in }
     }
     
     func testCurrentState() {
@@ -47,9 +52,86 @@ class Tests: XCTestCase {
     }
     
     func testEnumAllCases() {
-        let allCases = State.allCases
+        let allStates = State.all
         
-        assert(allCases == [.getUp, .eat, .sleep], "State.allCases: '\(allCases)' are't equal to '[.getUp, .eat, .sleep]'")
+        assert(allStates == [.getUp, .eat, .sleep], "State.all: '\(allStates)' are't equal to '[.getUp, .eat, .sleep]'")
     }
     
+    func testFlow() {
+        var string = ""
+        
+        sprocket.before { (from, to) in
+            string += "[Before] \(from.string) to \(to.string) "
+        }
+        
+        sprocket.on { (from, to) in
+            string += "[On] \(from.string) to \(to.string) "
+        }
+        
+        sprocket.after { (from, to) in
+            string += "[After] \(from.string) to \(to.string) "
+        }
+        
+        sprocket.to(.eat)
+        assert(string == "[Before] \(State.getUp.string) to \(State.eat.string) [On] \(State.getUp.string) to \(State.eat.string) ")
+        
+        string = ""
+        sprocket.to(.sleep)
+        assert(string == "[After] \(State.getUp.string) to \(State.eat.string) [Before] \(State.eat.string) to \(State.sleep.string) [On] \(State.eat.string) to \(State.sleep.string) ")
+        
+        string = ""
+        sprocket.to(.getUp)
+        assert(string == "[After] \(State.eat.string) to \(State.sleep.string) [Before] \(State.sleep.string) to \(State.getUp.string) [On] \(State.sleep.string) to \(State.getUp.string) ")
+    }
+    
+    func testSingleFlow() {
+        var string = ""
+        
+        sprocket.before(.getUp) { (from) in
+            string += "[Before] \(from.string) to \(State.getUp.string) "
+        }
+        
+        sprocket.on(.getUp) { (from) in
+            string += "[On] \(from.string) to \(State.getUp.string) "
+        }
+        
+        sprocket.after(.getUp) { (from) in
+            string += "[After] \(from.string) to \(State.getUp.string) "
+        }
+        
+        sprocket.before(.eat) { (from) in
+            string += "[Before] \(from.string) to \(State.eat.string) "
+        }
+        
+        sprocket.on(.eat) { (from) in
+            string += "[On] \(from.string) to \(State.eat.string) "
+        }
+        
+        sprocket.after(.eat) { (from) in
+            string += "[After] \(from.string) to \(State.eat.string) "
+        }
+        
+        sprocket.before(.sleep) { (from) in
+            string += "[Before] \(from.string) to \(State.sleep.string) "
+        }
+        
+        sprocket.on(.sleep) { (from) in
+            string += "[On] \(from.string) to \(State.sleep.string) "
+        }
+        
+        sprocket.after(.sleep) { (from) in
+            string += "[After] \(from.string) to \(State.sleep.string) "
+        }
+        
+        sprocket.to(.eat)
+        assert(string == "[Before] \(State.getUp.string) to \(State.eat.string) [On] \(State.getUp.string) to \(State.eat.string) ")
+        
+        string = ""
+        sprocket.to(.sleep)
+        assert(string == "[After] \(State.getUp.string) to \(State.eat.string) [Before] \(State.eat.string) to \(State.sleep.string) [On] \(State.eat.string) to \(State.sleep.string) ")
+        
+        string = ""
+        sprocket.to(.getUp)
+        assert(string == "[After] \(State.eat.string) to \(State.sleep.string) [Before] \(State.sleep.string) to \(State.getUp.string) [On] \(State.sleep.string) to \(State.getUp.string) ")
+    }
 }
